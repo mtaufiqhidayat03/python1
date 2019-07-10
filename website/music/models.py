07e1403bc0ceb697dfc03bd5d9f476bda9c6e9fd
mtaufiqhidayat03 <mtaufiq.hidayat03@gmail.com>
2019-07-10 16:03:34 +0700

    first commit

---
+++ website/music/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Song(models.Model):
+genre_choices = (
+		('Pop', 'Pop'),
+        ('Reggae', 'Reggae'),
+        ('Country', 'Country'),
+        ('Jazz', 'Jazz'),
+        ('Hip Hop', 'Hip Hop'),
+        ('Rock', 'Rock'),
+        ('R&B and Souls', 'R&B and Souls'),
+        ('Dangdut', 'Dangdut'),
+)
+title = models.CharField(max_length=50)
+genre = models.CharField(max_length=25, choices = genre_choices, default='Pop')
+singer = models.CharField(max_length=50)
+rating = models.PositiveIntegerField(default=1)
+
+def __unicode__(self):
+	return self.title
